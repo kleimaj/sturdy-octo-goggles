56520b54da15fbabfde0fda96aef159a049de8d0
kleimaj <jacobakleiman@gmail.com>
2021-01-30 23:40:45 -0800

    adds additional printline, adds semantic comments

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -5,14 +5,14 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     PrintLine(TEXT("Welcome to Bull Cows!\n"));
+    PrintLine(TEXT("Guess the 4 letter word!\n")); // modularize this  into a variable
     PrintLine(TEXT("Press enter to continue..."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = TEXT("cake");
-    PrintLine(Input);
+    FString HiddenWord = TEXT("cake"); // Move outside this function
     if (Input == HiddenWord) {
         PrintLine(TEXT("Correct Word"));
     }
