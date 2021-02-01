99815609e375ac7badce523796076f056c5a1e4c
kleimaj <jacobakleiman@gmail.com>
2021-01-31 18:42:35 -0800

    adds HiddenWord to header file

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -1,18 +1,19 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
 
+
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     PrintLine(TEXT("Welcome to Bull Cows!\n"));
     PrintLine(TEXT("Guess the 4 letter word!\n")); // modularize this  into a variable
     PrintLine(TEXT("Press enter to continue..."));
+    HiddenWord = TEXT("cake");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = TEXT("cake"); // Move outside this function
     if (Input == HiddenWord) {
         PrintLine(TEXT("Correct Word"));
     }
