9ec21a3d90f6aca8cc05ac02a5c88b92c75b535a
kleimaj <jacobakleiman@gmail.com>
2021-02-01 13:48:27 -0800

    optimizes IsIsogram()

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -34,12 +34,12 @@ void UBullCowCartridge::ProcessGuess(FString Guess)
         EndGame();
         return;
     }
-    if (!IsIsogram(Guess)) {
-        PrintLine(TEXT("No repeating letters, guess again!"));
-        return;
-    }
     if (Guess.Len() != HiddenWord.Len()) {
         PrintLine(TEXT("Sorry, try guessing again! \nThe Hidden Word is %i characters long!"), HiddenWord.Len());
+        return;
+    }
+    if (!IsIsogram(Guess)) {
+        PrintLine(TEXT("No repeating letters, guess again!"));
         return;
     }
 
@@ -58,19 +58,14 @@ void UBullCowCartridge::ProcessGuess(FString Guess)
 
 bool UBullCowCartridge::IsIsogram(FString Word)
 {
-    int32 count = 0;
-    int32 len = Word.Len();
-    for (int i = 0; i < len; i++) {
-        char curr = Word[i];
-        for (int j = 0; j < len; j++) {
-            if (curr == Word[j]) {
-                count++;
-            }
-            if (count == 2) {
+    int32 Len = Word.Len();
+    for (int i = 0; i < Len; i++) {
+        char Curr = Word[i];
+        for (int j = i+1; j < Len; j++) {
+            if (Curr == Word[j]) {
                 return false;
             }
         }
-        count = 0;
     }
     return true;
 }
@@ -86,9 +81,6 @@ void UBullCowCartridge::InitGame()
     PrintLine(TEXT("Guess the %i letter word!\n"), HiddenWord.Len());
     PrintLine(TEXT("You have %i Lives"), Lives);
     PrintLine(TEXT("Type in your guess. \nPress enter to continue..."));
-
-    const TCHAR HW[] = TEXT("cakes");
-    PrintLine(TEXT("Character 1 of the hiddeon word is %c"), HW[0]);
 }
 
 void UBullCowCartridge::EndGame() 
